56bf6350c9f1f0d672744ba1429120f4db40df2c
Hank Barta <hbarta@gmail.com>
2016-11-16 18:16:43 -0600

    working on test cases

--- s20/s20.go
+++ s20/s20.go
@@ -23,5 +23,17 @@ ON = b'\x01'
 OFF = b'\x00'
 */
 const MAGIC = []byte { 0x68, 0x64 }
-DISCOVERY = b'\x00\x06\x71\x61'
+const DISCOVERY = []byte {0x00, 0x06, 0x71, 0x61}
+const DISCOVERY_RESP = []byte {0x00, 0x2a, 0x71, 0x61}
+const SUBSCRIBE = []byte {x00, 0x1e\x63\x6c}
+const SUBSCRIBE_RESP = []byte {x00, 0x18\x63\x6c}
+const CONTROL = []byte {x00, 0x17, 0x64, 0x63}
+const CONTROL_RESP = []byte {x00, 0x17, 0x73, 0x66}
+const PADDING_1 = []byte {x20, 0x20, 0x20, 0x20, 0x20, 0x20}
+const PADDING_2 = []byte {x00, 0x00, 0x00, 0x00}
+const ON = []byte {x01}
+const OFF = []byte {x00}
 
+func Good() (int) {
+	return 1
+}
--- s20/s20_test.go
+++ s20/s20_test.go
@@ -2,7 +2,21 @@ package main
 
 import (
 	"github.com/HankB/orvibo/s20"
+	"fmt"
+	"testing"
 )
 
+
+
 func main() {
+	fmt.Println("Hello")
+        // Output:
+        // Hello
 }
+
+func TestXxx(*testing.T) {o
+	if s20.Good() != 1 {
+		Fail("not good")
+	}
+}
+
