6a516486c99cc816f3c1ce1991d5899e42e24ad7
Hank Barta <hbarta@gmail.com>
2016-11-24 00:03:29 -0600

    moved Dump and related to txtutil

--- s20/s20.go
+++ s20/s20.go
@@ -1,8 +1,6 @@
 // Package to support operations involving the Orvibo S20
 package s20
 
-import "fmt"
-
 //"net"
 //"os"
 //"strconv"
@@ -37,48 +35,4 @@ const OFF = "\x00"
 
 func Good() int {
 	return 1
-}
-
-// Format output similarly to 'hexdump -D'
-func Dump(b string) {
-	chars := 0 // chars printed
-	asciiRep := []byte{}
-	for i := 0; i < len(b); i++ { // iterate over entire array
-		if (chars % 16) == 0 { // starting a full row?
-			if chars > 0 { // need to print end of previous line?
-				for i := 0; (i+chars)%16 != 0; i++ { // fill out line < 16 bytes
-					fmt.Printf("   ")
-					asciiRep = append(asciiRep, ' ')
-					if chars < 8 {
-						asciiRep = append(asciiRep, ' ')
-					}
-					fmt.Printf(" |%s|\n", asciiRep)
-					asciiRep = []byte{}
-				}
-			}
-			fmt.Printf("%8.8x  ", chars)
-		} else if (chars % 8) == 0 { // mid row?
-			fmt.Printf(" ") // mid row space
-		}
-
-		// add the hex representation to the output
-		fmt.Printf("%2.2x ", b[i])
-		// add the ASCII rep (if printable)
-		if IsPrint(b[i]) {
-			asciiRep = append(asciiRep, b[i])
-		} else {
-			asciiRep = append(asciiRep, '.')
-		}
-
-		chars++
-	}
-}
-
-//}
-
-func IsPrint(b byte) bool {
-	if b >= ' ' && b <= '~' {
-		return true
-	}
-	return false
 }
--- s20/s20_test.go
+++ s20/s20_test.go
@@ -1,43 +1,12 @@
-package s20
+package s20_test
 
 import (
-	"fmt"
-	"testing"
-
 	s20 "github.com/HankB/orvibo/s20"
+	txt "github.com/HankB/txtutil"
 )
 
-func main() {
-	fmt.Println("Hello")
-	// Output:
-	// Hello
-
-	s20.Dump(s20.MAGIC)
-	// Output:
-	// [ 86 64 ]
-}
-
-func TestIsPrint(t *testing.T) {
-	if s20.IsPrint(10) {
-		t.Fail()
-	}
-
-	if s20.IsPrint(0x20) {
-		t.Fail()
-	}
-}
-
-func TestDump(t *testing.T) {
-	// this test does not work
-	s20.Dump(s20.MAGIC)
-	// Output:
-	// [ 86 64 ]
-	s20.Dump(s20.SUBSCRIBE)
-	s20.Dump(s20.SUBSCRIBE + s20.CONTROL_RESP + s20.CONTROL_RESP + s20.CONTROL_RESP + s20.CONTROL_RESP)
-}
-
 func ExampleDump() {
-	s20.Dump(s20.MAGIC)
-	// Output:
-	// [ 68 64 ]
+	txt.Dump(s20.MAGIC)
+	// 00000000  68 64                                             |hd|
+	// 00000002
 }
