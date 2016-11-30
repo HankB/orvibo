4306cf137f9774261d5afda26b336daaa67b0c30
Hank Barta <hbarta@gmail.com>
2016-11-30 14:03:00 -0600

    Sending discover S20 is sending reply 10x

--- s20/cmd.go
+++ s20/cmd.go
@@ -27,26 +27,24 @@ func Discover(timeout int) ([]string, error) {
 	var readLen int
 	var fromAddr *net.UDPAddr
 
-	// get server handle
-	server := fmt.Sprintf("%s:%d", bcastIP, udpDiscoverPort)
+	// get server connection
+	server := fmt.Sprintf("%s:%d", bcastIP, udpDiscoverPort) // "255.255.255.255", 10000
 	serverAddr, err = net.ResolveUDPAddr("udp", server)
 	checkErr(err)
-	// client := fmt.Sprintf("%s:%d", "0.0.0.0", udpDiscoverPort)
-	// client := fmt.Sprintf(":%d", udpDiscoverPort)
 	ourAddr, err = net.ResolveUDPAddr("udp", "192.168.1.132:10000")
 	checkErr(err)
-	// fmt.Println("calling DialUDP()")
 	conn, err = net.DialUDP("udp", ourAddr, serverAddr)
 	checkErr(err)
 	defer conn.Close()
-	// fmt.Println("Established UDP socket")
 
+	// send the Discover message
 	discoverMsg := []byte(magic)
 	discoverMsg = append(discoverMsg, discovery...)
 	sendLen, err := conn.Write(discoverMsg)
 	checkErr(err)
 	fmt.Println("Sent", sendLen, "bytes")
-	// send the Discover message
+
+	// read one reply
 	readLen, fromAddr, err = conn.ReadFromUDP(inBuf)
 	fmt.Println("Read ", readLen, "bytesfrom ", fromAddr)
 	txtutil.Dump(string(inBuf[:readLen]))
