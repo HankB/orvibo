357c7ce1db74f1d9a5a80edbc46d425fced7c4ac
Hank Barta <hbarta@gmail.com>
2016-12-01 16:40:19 -0600

    added module ID and some cleanup

--- s20/s20.go
+++ s20/s20.go
@@ -35,21 +35,22 @@ const padding2 = "\x00\x00\x00\x00"
 const on = "\x01"
 const off = "\x00"
 
-var ssid = "" // SSID we will pair with
-// var ip = ""    // IP address used for pairing
-var pwd = ""                    // password for SSID.
-var swStr = ""                  // string used to establish connection
-var s20IP = "10.10.100.254"     // IP address used by the S20
-var ourIP = "10.10.100.150"     // IP address S20 will assign to host
-var bcastIP = "255.255.255.255" // broadcast IP address
+var ssid = ""  // SSID we will pair with
+var pwd = ""   // password for SSID.
+var swStr = "" // string used to establish connection
+var mid = ""   // name module will use
+
+const s20IP = "10.10.100.254"     // IP address used by the S20
+const ourIP = "10.10.100.150"     // IP address S20 will assign to host
+const bcastIP = "255.255.255.255" // broadcast IP address
 
 const udpRcvPort = 9884       // port we listen on when pairing
 const udpSndPort = 48899      // port S20 listens on when pairing
 const udpDiscoverPort = 10000 // port to send discovery message
 
-// Init saves newtwork parameters for later usage and
+// Init saves network parameters for later usage and
 // opens the port
-func Init(SSID string, password string) {
+func Init(SSID string, password string, module_ID string) {
 	ssid = SSID
 	// ip = IP
 	pwd = password
