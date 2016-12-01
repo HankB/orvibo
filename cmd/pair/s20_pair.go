eaeeb035c6b8f1771b57342bff2f77dde24bd8f1
Hank Barta <hbarta@gmail.com>
2016-12-01 16:41:49 -0600

    added module ID

--- cmd/pair/s20_pair.go
+++ cmd/pair/s20_pair.go
@@ -13,16 +13,17 @@ func usage(progname string, description string) {
 	if description != "" {
 		fmt.Println(description)
 	}
-	fmt.Println("Usage:", progname, "SSID password")
+	fmt.Println("Usage:", progname, "SSID password module_ID")
 	os.Exit(1)
 }
 
 func main() {
 	args := os.Args
-	if len(args) != 3 {
+	if len(args) != 4 {
 		usage(args[0], "")
 	}
-	fmt.Printf("Associating with \"%s\" using \"%s\"\n", args[1], args[2])
-	s20.Init(args[1], args[2])
+	fmt.Printf("Associating with \"%s\" using \"%s\" naming as \"%s\"\n",
+		args[1], args[2], args[3])
+	s20.Init(args[1], args[2], args[3])
 	s20.Pair()
 }
