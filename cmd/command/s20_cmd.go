f20920bb9b13dbc834dc70f96abf68a7140df807
Hank Barta <hbarta@gmail.com>
2017-04-14 10:57:16 -0500

    implement simple command line arguments

--- cmd/command/s20_cmd.go
+++ cmd/command/s20_cmd.go
@@ -16,9 +16,16 @@ func usage(errstr string) {
 }
 func main() {
 
-	s20s, _ := s20.Discover(10)
-	for _, s20 := range s20s {
-		fmt.Println(s20)
+	if len(os.Args) == 1 {
+		fmt.Println("Usage", os.Args[0])
+		os.Exit(0)
+	} else if os.Args[1] == "-d" {
+		s20s, _ := s20.Discover(2)
+		for _, s20 := range s20s {
+			fmt.Println(s20)
+		}
+		os.Exit(0)
 	}
-	os.Exit(0)
+	fmt.Println("Unknown command arg", os.Args[1])
+	os.Exit(1)
 }
