ae07215c0a6d962588169e442b75979506394d47
Hank Barta <hbarta@gmail.com>
2017-04-14 20:43:03 -0500

    added code to call s2.Discover()

--- cmd/command/s20_cmd.go
+++ cmd/command/s20_cmd.go
@@ -8,6 +8,8 @@ import (
 	"github.com/HankB/orvibo/s20"
 )
 
+const replyTimeout = 2 // reply timeout in seconds
+
 func usage(errstr string) {
 	if len(errstr) > 0 {
 		fmt.Println(errstr)
@@ -20,10 +22,15 @@ func main() {
 		fmt.Println("Usage", os.Args[0])
 		os.Exit(0)
 	} else if os.Args[1] == "-d" {
-		s20s, _ := s20.Discover(2)
-		for _, s20 := range s20s {
-			fmt.Println(s20)
+		s20s, _ := s20.Discover(replyTimeout)
+		for _, s20s := range s20s {
+			fmt.Println(s20s)
 		}
+		os.Exit(0)
+	} else if os.Args[1] == "-s" {
+		s20s, _ := s20.Discover(replyTimeout)
+		e := s20.Subscribe(replyTimeout, s20s[0].IpAddr)
+		fmt.Println("done", e)
 		os.Exit(0)
 	}
 	fmt.Println("Unknown command arg", os.Args[1])
