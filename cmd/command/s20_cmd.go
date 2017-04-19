cd65fef1f6b85248e4a8287e1cd6947b8286f6c3
Hank Barta <hbarta@gmail.com>
2017-04-19 15:46:33 -0500

    added code to send on/off command to first S20

--- cmd/command/s20_cmd.go
+++ cmd/command/s20_cmd.go
@@ -20,7 +20,7 @@ func main() {
 	// did the user pass arguments?
 
 	if len(os.Args) == 1 {
-		fmt.Println("Usage", os.Args[0], "[-d]|[-s]")
+		fmt.Println("Usage", os.Args[0], "[-d]|[-s][-c]")
 		os.Exit(0)
 	} else if os.Args[1] == "-d" { // exercise Discover command
 		s20s, _ := s20.Discover(replyTimeout)
@@ -46,6 +46,36 @@ func main() {
 		} else {
 			fmt.Println("subscription error:", e)
 		}
+		os.Exit(0)
+	} else if os.Args[1] == "-c" { // test Subscribe (first discovered S20)
+		s20s, _ := s20.Discover(replyTimeout)
+		var cmd bool
+		var state string
+
+		if len(s20s) < 1 {
+			fmt.Println("No S20s discovered")
+			os.Exit(0)
+		}
+		e := s20.Subscribe(replyTimeout, &s20s[0])
+		if e == nil {
+			if s20s[0].IsOn {
+				state = "on"
+				cmd = false
+			} else {
+				state = "off"
+				cmd = true
+			}
+			fmt.Println("subscribed and currently", state)
+		} else {
+			fmt.Println("subscription error:", e)
+		}
+		e = s20.Control(replyTimeout, &s20s[0], cmd)
+		if s20s[0].IsOn {
+			state = "on"
+		} else {
+			state = "off"
+		}
+		fmt.Println("commanded and currently", state)
 		os.Exit(0)
 	}
 	fmt.Println("Unknown command arg", os.Args[1])
