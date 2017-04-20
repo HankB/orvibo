6972f9dad284701b3711238a0af19eb74ae7594c
Hank Barta <hbarta@gmail.com>
2017-04-20 14:55:08 -0500

    implement Control of named device (and better usage()

--- cmd/command/s20_cmd.go
+++ cmd/command/s20_cmd.go
@@ -2,26 +2,42 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"net"
 	"os"
 
 	"github.com/HankB/orvibo/s20"
 )
 
-const replyTimeout = 2 // reply timeout in seconds
+const replyTimeout = 1 // reply timeout in seconds
+var s20s []s20.Device
 
 func usage(errstr string) {
 	if len(errstr) > 0 {
 		fmt.Println(errstr)
 	}
-
+	fmt.Println("Usage", os.Args[0], "[-d]|[-s][-c [host|IP addr] [on|off]]")
+	os.Exit(0)
 }
-func main() {
-	// did the user pass arguments?
 
+func findDeviceByIP(addr *net.IPAddr) (s20.Device, error) {
+	for _, device := range s20s {
+		if bytes.Compare(addr.IP, device.IPAddr.IP) == 0 {
+			return device, nil
+		}
+	}
+	return s20s[0], errors.New("IP not found in discover list")
+}
+
+func main() {
+	var cmd bool
+	var state string
+
+	// did the user pass arguments?
 	if len(os.Args) == 1 {
-		fmt.Println("Usage", os.Args[0], "[-d]|[-s][-c]")
-		os.Exit(0)
+		usage("")
 	} else if os.Args[1] == "-d" { // exercise Discover command
 		s20s, _ := s20.Discover(replyTimeout)
 		for _, s20ds := range s20s {
@@ -31,8 +47,7 @@ func main() {
 	} else if os.Args[1] == "-s" { // test Subscribe (first discovered S20)
 		s20s, _ := s20.Discover(replyTimeout)
 		if len(s20s) < 1 {
-			fmt.Println("No S20s discovered")
-			os.Exit(0)
+			usage("No S20s discovered")
 		}
 		e := s20.Subscribe(replyTimeout, &s20s[0])
 		if e == nil {
@@ -47,35 +62,56 @@ func main() {
 			fmt.Println("subscription error:", e)
 		}
 		os.Exit(0)
-	} else if os.Args[1] == "-c" { // test Subscribe (first discovered S20)
-		s20s, _ := s20.Discover(replyTimeout)
-		var cmd bool
-		var state string
+	} else if os.Args[1] == "-c" { // test Control (first discovered S20)
+		if len(os.Args) < 4 {
+			usage("")
+		}
 
+		// unpack on/off command
+		if os.Args[3] == "on" {
+			cmd = true
+		} else if os.Args[3] == "off" {
+			cmd = false
+		} else {
+			usage("command must be \"on\" or \"off\" (found \"" + os.Args[3] + "\")")
+		}
+
+		// resolve the IP address provided
+		ipAddr, err := net.ResolveIPAddr("ip", os.Args[2])
+		if err != nil {
+			usage("Cannot resolve " + os.Args[2])
+		}
+		fmt.Println("resolved as ", ipAddr)
+
+		s20s, _ = s20.Discover(replyTimeout)
+
+		dev, err := findDeviceByIP(ipAddr)
 		if len(s20s) < 1 {
 			fmt.Println("No S20s discovered")
 			os.Exit(0)
 		}
-		e := s20.Subscribe(replyTimeout, &s20s[0])
+		e := s20.Subscribe(replyTimeout, &dev)
 		if e == nil {
-			if s20s[0].IsOn {
+			if dev.IsOn {
 				state = "on"
-				cmd = false
 			} else {
 				state = "off"
-				cmd = true
 			}
 			fmt.Println("subscribed and currently", state)
 		} else {
 			fmt.Println("subscription error:", e)
 		}
-		e = s20.Control(replyTimeout, &s20s[0], cmd)
-		if s20s[0].IsOn {
-			state = "on"
+		e = s20.Control(replyTimeout, &dev, cmd)
+		if e == nil {
+			if dev.IsOn {
+				state = "on"
+			} else {
+				state = "off"
+			}
+			fmt.Println("commanded and currently", state)
 		} else {
-			state = "off"
+			fmt.Println("control error:", e)
 		}
-		fmt.Println("commanded and currently", state)
 		os.Exit(0)
 	}
 	fmt.Println("Unknown command arg", os.Args[1])
